2f257307ff9a25f441911f705f0e78454054d118
oladolapo <66223952+oladolapo@users.noreply.github.com>
2021-11-08 23:57:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+var express = require('express');
+const user = require("./controller/user");
+const {loginAuthorization} = require("./middleware/authorization");
+const morgan = require("morgan");
+const path = require('path');
+const mongoose = require('mongoose');
+
+// loads environment variables from .env file
+require('dotenv/config');
+
+//connect to mongodb
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+    if (err) console.log(err.message);
+    else console.log("connected to mongodb");
+});
+
+const upload = require("./utils/multerConfig");
+
+// initialize express to  app variable
+const app = express();
+
+// set the view engine to ejs
+// app.set('view engine', 'ejs'); 
+// app.set('views', './views');
+
+app.use(express.static("./views"));
+app.use(express.static(path.resolve('public')));
+app.use(morgan("dev"));
+/** MIDDLEWARE  Analogy **/
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// display the home page
+app.get('/', user.homepage);
+// displays the profile page
+app.get("/profile", user.profile);
+
+// uploads the user image
+app.post('/user/uploadPic', upload.single('avatar'), user.uploadImage);
+
+// creates a new user account
+app.post("/user/signup", user.createUser);
+
+// verifies the user account
+app.get("/user/verify", user.verifyAccount);
+
+// authenticates the user
+app.post('/user/login', user.signIn);
+
+// gets the user profile
+app.post('/profile', loginAuthorization, user.getProfile);
+
+app.patch('/profile', loginAuthorization, user.updateProfile);
+
+// deletes user account
+app.delete('/user', loginAuthorization, user.deleteUser);
+// sets the port to listening on 
+
+app.use(require("./routes/admin"));
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next) {
+    res.status(404).sendFile(require('path').join(__dirname, 'views', 'error.html'));
+});
+app.listen(app.get("port"), _ => console.log(`listening on port ${app.get("port")}`));
---
+++ model/database.js
@@ -0,0 +1,26 @@
+// exports.userDatabase = [{
+//     id: 1001,
+//     name: "dolapo",
+//     age: 23,
+//     address: "ikeja",
+//     photo: null
+// }, {
+//     id: 1002,
+//     name: "deji",
+//     age: 26,
+//     address: "magodo",
+//     photo: null
+// },
+// {
+//     id: 1003,
+//     name: "dotun",
+//     age: 30,
+//     address: "ogudu",
+//     photo: null
+// }
+// ];
+
+// exports.contactDatabase = []
+exports.userDatabase = [];
+
+exports.profileDatabase = [];
