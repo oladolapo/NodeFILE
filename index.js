e1e0bee8552f856a1ffb6e505fcfeb2c66e340d8
oladolapo <66223952+oladolapo@users.noreply.github.com>
2021-11-17 21:37:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,27 @@
-var express = require('express');
-const user = require("./controller/user");
-const {loginAuthorization} = require("./middleware/authorization");
-const morgan = require("morgan");
+const express = require("express");
 const path = require('path');
-const mongoose = require('mongoose');
-
-// loads environment variables from .env file
-require('dotenv/config');
-
-//connect to mongodb
-mongoose.connect(process.env.MONGODB_URI, (err) => {
-    if (err) console.log(err.message);
-    else console.log("connected to mongodb");
-});
-
-const upload = require("./utils/multerConfig");
-
-// initialize express to  app variable
+const user = require('./controllers/users');
 const app = express();
+const authorization= require('./middleware/authorization');
+const morgan = require('morgan');
 
-// set the view engine to ejs
-// app.set('view engine', 'ejs'); 
-// app.set('views', './views');
-
-app.use(express.static("./views"));
-app.use(express.static(path.resolve('public')));
-app.use(morgan("dev"));
-/** MIDDLEWARE  Analogy **/
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ urlEncoded: false }));
 
-// display the home page
+app.use(express.static("./views"));
+app.use(express.static(path.resolve("public")));
+app.use(morgan('dev'));
+
+
 app.get('/', user.homepage);
-// displays the profile page
 app.get("/profile", user.profile);
 
-// uploads the user image
-app.post('/user/uploadPic', upload.single('avatar'), user.uploadImage);
+app.post('/user/signup', user.createUser);
+app.post('/user/login', user.signin);
+app.post('/profile', authorization,user.getprofile);
+app.patch('/profile', authorization,user.updateprofile);
 
-// creates a new user account
-app.post("/user/signup", user.createUser);
-
-// verifies the user account
-app.get("/user/verify", user.verifyAccount);
-
-// authenticates the user
-app.post('/user/login', user.signIn);
-
-// gets the user profile
-app.post('/profile', loginAuthorization, user.getProfile);
-
-app.patch('/profile', loginAuthorization, user.updateProfile);
-
-// deletes user account
-app.delete('/user', loginAuthorization, user.deleteUser);
-// sets the port to listening on 
-
-app.use(require("./routes/admin"));
-
-app.set('port', process.env.PORT || 3000);
-
-app.use(function(req, res, next) {
-    res.status(404).sendFile(require('path').join(__dirname, 'views', 'error.html'));
-});
-app.listen(app.get("port"), _ => console.log(`listening on port ${app.get("port")}`));
+app.listen(3000, () => {
+  console.log("app is running")
+})
